0f9ecf5e5e5bfe5a9db78aad1db474e2e6fea964
Rafael Oliveira <ra.pha@live.com>
2023-10-23 20:10:41 -0300

    lint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const puppeteer = require('puppeteer')
-const fs = require('fs')
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 // Uncomment the following line if increasing parallel downloads is desired
 // require('events').EventEmitter.defaultMaxListeners = 15;
-const parallelDownloads = 9
+const parallelDownloads = 9;
 let downloadBasePath;
 
 function sleep(ms) {
@@ -12,116 +12,116 @@ function sleep(ms) {
 
 function lookup(obj, k) {
     for (let key in obj) {
-        const value = obj[key]
+        const value = obj[key];
 
         if (k == key) {
-            return [k, value]
+            return [k, value];
         }
 
         if (typeof (value) === "object" && !Array.isArray(value)) {
-            const y = lookup(value, k)
-            if (y && y[0] == k) return y
+            const y = lookup(value, k);
+            if (y && y[0] == k) return y;
         }
         if (Array.isArray(value)) {
             for (let i = 0; i < value.length; ++i) {
-                const x = lookup(value[i], k)
-                if (x && x[0] == k) return x
+                const x = lookup(value[i], k);
+                if (x && x[0] == k) return x;
             }
         }
     }
 
-    return null
+    return null;
 }
 
 // not being currently used, proven to be inconsistent
-const getDownloadLinkByClicks = async (link) => {
+const getDownloadLinkByClicks = async link => {
     const browser = await puppeteer.launch({
         product: 'chrome',
         headless: true,
     });
-    await sleep(500)
-    let page = await browser.newPage()
-    await page.goto(link)
-    await sleep(2000)
+    await sleep(500);
+    let page = await browser.newPage();
+    await page.goto(link);
+    await sleep(2000);
 
     // first click opens ad page
-    let errNoClass = false
+    let errNoClass = false;
     try {
         // lol
-        await page.click(".DownloadButton_ButtonSoScraperCanTakeThisName__gVd3C")
+        await page.click(".DownloadButton_ButtonSoScraperCanTakeThisName__gVd3C");
     } catch (error) {
-        console.log("with button no class")
-        errNoClass = true
+        console.log("with button no class");
+        errNoClass = true;
     }
 
-    let unlockButtonHandler = []
+    let unlockButtonHandler = [];
 
     if (errNoClass) {
         for (let i = 15; i < 40; i++) {
 
 
-            d = await page.$$(`body > div:nth-child(${i})`)
+            d = await page.$$(`body > div:nth-child(${i})`);
 
             if (d.length !== 0) {
-                unlockButtonHandler.push(d[0])
+                unlockButtonHandler.push(d[0]);
                 try {
-                    await d[0].click()
+                    await d[0].click();
 
                 } catch (error) {
-                    console.log("not clickable")
+                    console.log("not clickable");
                 }
             }
         }
     }
 
 
-    await sleep(3000)
+    await sleep(3000);
 
     // Closing ad page
-    let pages = await browser.pages()
+    let pages = await browser.pages();
 
-    let adPage = pages[2]
-    await adPage.close()
-    await sleep(5000)
+    let adPage = pages[2];
+    await adPage.close();
+    await sleep(5000);
 
     // Unlocks download button
     if (errNoClass) {
         for (let el of unlockButtonHandler) {
-            el.click()
+            el.click();
         }
     } else {
-        await page.click(".DownloadButton_ButtonSoScraperCanTakeThisName__gVd3C")
+        await page.click(".DownloadButton_ButtonSoScraperCanTakeThisName__gVd3C");
     }
 
     // Find and return download link
-    await sleep(2500)
+    await sleep(2500);
 
-    downloadHandle = await page.$(".DownloadButton_DownloadButton__qwe2g ")
-    const downloadLink = await page.evaluate(element => element.getAttribute('href'), downloadHandle)
+    downloadHandle = await page.$(".DownloadButton_DownloadButton__qwe2g ");
+    const downloadLink = await page.evaluate(element => element.getAttribute('href'), downloadHandle);
 
-    await browser.close()
+    await browser.close();
 
-    return downloadLink
-}
+    return downloadLink;
+};
 
 const getDownloadLinkByScriptTag = async link => {
     const browser = await puppeteer.launch({
         product: 'chrome',
         headless: "new",
     });
-    await sleep(500)
-    let page = await browser.newPage()
-    await page.goto(link)
-    await sleep(1000)
+    await sleep(500);
+    let page = await browser.newPage();
+    await page.goto(link);
+    await sleep(1000);
 
 
     // find file name
-    fileNameHandle = await page.$(".page_TextHeading__VsM7r")
-    const fileName = await page.evaluate(element => element.textContent, fileNameHandle)
+    fileNameHandle = await page.$(".page_TextHeading__VsM7r");
+    const fileName = await page.evaluate(element => element.textContent, fileNameHandle);
 
     // Use page.$$eval to get all script tags
-    const scriptTags = await page.$$eval('script', (elements) => {
-        return elements.map((element) => {
+    const scriptTags = await page.$$eval('script', elements => {
+        return elements.map(element => {
             return {
                 src: element.getAttribute('src'),
                 content: element.innerHTML,
@@ -132,21 +132,21 @@ const getDownloadLinkByScriptTag = async link => {
 
     for (let tag of scriptTags) {
         if (tag.content == null) {
-            return
+            return;
         }
         if (tag.content.includes('fileExtension')) {
             // Remove 26 initial characters
-            const step1 = tag.content.substring(25)
+            const step1 = tag.content.substring(25);
 
             // Remove the last character
-            const step2 = step1.slice(0, -1)
+            const step2 = step1.slice(0, -1);
 
             // Remove trailing quotes if present
-            const step3 = step2.replace(/"$/, '').replace(/^"/, '')
+            const step3 = step2.replace(/"$/, '').replace(/^"/, '');
 
-            const step4 = step3.replace(/\\"/g, '"')
+            const step4 = step3.replace(/\\"/g, '"');
 
-            const step5 = step4.slice(0, -4)
+            const step5 = step4.slice(0, -4);
 
             // Parse as JSON
 
@@ -160,16 +160,16 @@ const getDownloadLinkByScriptTag = async link => {
             console.error('Error parsing JSON:', error);
         }
     }
-}
+};
 
 async function traxLinkGetter() {
     if (process.argv.length < 4 || process.argv.length > 5) {
-        console.log("Usage: <download link> <release date> [<download folder>]")
-        return
+        console.log("Usage: <download link> <release date> [<download folder>]");
+        return;
     }
-    const release = process.argv[2]
-    const link = process.argv[3]
-    downloadBasePath = process.argv[4] || process.platform === "win32" ? 'C:\\Music' : "~/Music"
+    const release = process.argv[2];
+    const link = process.argv[3];
+    downloadBasePath = process.argv[4] || process.platform === "win32" ? 'C:\\Music' : "~/Music";
 
     const downloadPath = downloadBasePath + release;
 
@@ -178,13 +178,13 @@ async function traxLinkGetter() {
         headless: false, // or true if you don't want to see the browser
     });
 
-    let page = await browser.newPage()
+    let page = await browser.newPage();
 
 
-    await page.goto(link)
-    await sleep(1500)
+    await page.goto(link);
+    await sleep(1500);
 
-    const unlockDownloadHandle = await page.$$(".DownloadButton_DownloadButton__Co0Pm")
+    const unlockDownloadHandle = await page.$$(".DownloadButton_DownloadButton__Co0Pm");
 
     let links = unlockDownloadHandle
         .map(async elementHandle => await page.evaluate(element => element.getAttribute('href'), elementHandle));
@@ -196,59 +196,59 @@ async function traxLinkGetter() {
 
     let curr = 0;
     for (let i = 0; i < preDownloadLinks.length; i++) {
-        downloadLinksPromises.push(getDownloadLinkByScriptTag(preDownloadLinks[i]))
-        curr++
+        downloadLinksPromises.push(getDownloadLinkByScriptTag(preDownloadLinks[i]));
+        curr++;
         if (curr >= parallelDownloads) {
-            links = await Promise.all(downloadLinksPromises)
-            downloadLinks.push(links)
-            curr = 0
-            downloadLinksPromises = []
+            links = await Promise.all(downloadLinksPromises);
+            downloadLinks.push(links);
+            curr = 0;
+            downloadLinksPromises = [];
         }
     }
 
     if (curr !== 0) {
-        links = await Promise.all(downloadLinksPromises)
-        downloadLinks.push(links)
+        links = await Promise.all(downloadLinksPromises);
+        downloadLinks.push(links);
     }
 
     downloadLinks = downloadLinks.flat();
 
-    await browser.close()
+    await browser.close();
 
-    fs.mkdirSync(downloadPath, { recursive: true })
-    const writeStream = fs.createWriteStream(downloadPath + "\\links.txt", { flags: 'w' })
+    fs.mkdirSync(downloadPath, { recursive: true });
+    const writeStream = fs.createWriteStream(downloadPath + "\\links.txt", { flags: 'w' });
 
     downloadLinks.forEach((line) => {
-        writeStream.write(line + '\n')
+        writeStream.write(line + '\n');
     });
 
-    writeStream.end()
+    writeStream.end();
 
     writeStream.on('finish', () => {
-        console.log('Created links file:', downloadPath + "\\links.txt")
+        console.log('Created links file:', downloadPath + "\\links.txt");
     });
 
     writeStream.on('error', (err) => {
-        console.error('Error writing to file:', err)
+        console.error('Error writing to file:', err);
     });
 
     // Copy downloader script to release folder
     fs.readFile("downloader.sh", (err, data) => {
         if (err) {
-            console.error('Error reading the source file:', err)
-            return
+            console.error('Error reading the source file:', err);
+            return;
         }
 
         fs.writeFile(downloadPath + "\\downloader.sh", data, (err) => {
             if (err) {
-                console.error('Error writing the destination file:', err)
+                console.error('Error writing the destination file:', err);
                 return;
             }
 
-            console.log('Copied download script file to:', downloadPath)
-        })
-    })
+            console.log('Copied download script file to:', downloadPath);
+        });
+    });
 
 }
 
-traxLinkGetter()
+traxLinkGetter();
